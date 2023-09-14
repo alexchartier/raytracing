Fixes to quiver_example. Rotation is correct now

df_code/my_utils.py
@@ -203,21 +203,43 @@ def bearing_to_xy_comp(bearing:np.array):
         th[i] = theta  
     return x,y,th
 #_______________________________________________________________________________
-def geodetic_latlonz_to_ecef(lat:np.array,lon:np.array,z:np.array,deg=True):
-    # convert geodetic (lat,lon,z) to ECEF (x,y,z) using nvector  
-    wgs84  = nv.FrameE(name='WGS84') 
-    N      = len(lat)
-    x_ecef = np.zeros(N) 
-    y_ecef = np.zeros(N) 
-    z_ecef = np.zeros(N) 
-    for i in range(N): 
-        p         = wgs84.GeoPoint(latitude=lat[i],longitude=lon[i],z=z[i],degrees=deg) 
+def geodetic_latlonz_to_ecef(lat:np.array,lon:np.array,z:np.array,deg=True,scale='m'):
+    # convert geodetic (lat,lon,z) to ECEF (x,y,z) using nvector
+    sf = 1.
+    if scale=='km': 
+        sf = 1e-3 
+    wgs84 = nv.FrameE(name='WGS84')
+    N = len(lat) - 1
+    XYZ = np.zeros((N, 3))
+    UVW = np.zeros((N, 3))
+    for i in range(N):
+        p  = wgs84.GeoPoint(latitude=lat[i]  ,longitude=lon[i]  ,z=z[i]  ,degrees=deg)
+        p2 = wgs84.GeoPoint(latitude=lat[i+1],longitude=lon[i+1],z=z[i+1],degrees=deg)
+        # compute the relative distance to the next (lat,lon,z) point
+        brng      = p.delta_to(p2)     # can effectively turn this into a bearing angle by calling .azimuth_deg
+        # convert to ECEF frame
         p_ecef    = p.to_ecef_vector()
-        pv        = p_ecef.ravel().to_list()  
-        x_ecef[i] = pv[0]  
-        y_ecef[i] = pv[1]  
-        z_ecef[i] = pv[2] 
-    return x_ecef,y_ecef,z_ecef 
+        brng_ecef = brng.to_ecef_vector()
+        # fill the output
+        XYZ[i, :] = sf*p_ecef.pvector[:].flatten()      # starting point of vector (x0,y0,z0)  
+        UVW[i, :] = sf*brng_ecef.pvector[:].flatten()   # magnitude of components/direction of vector (u,v,w) 
+    return XYZ,UVW
+# #_______________________________________________________________________________
+# def geodetic_latlonz_to_ecef(lat:np.array,lon:np.array,z:np.array,deg=True):
+#     # convert geodetic (lat,lon,z) to ECEF (x,y,z) using nvector  
+#     wgs84  = nv.FrameE(name='WGS84') 
+#     N      = len(lat)
+#     x_ecef = np.zeros(N) 
+#     y_ecef = np.zeros(N) 
+#     z_ecef = np.zeros(N) 
+#     for i in range(N): 
+#         p         = wgs84.GeoPoint(latitude=lat[i],longitude=lon[i],z=z[i],degrees=deg) 
+#         p_ecef    = p.to_ecef_vector()
+#         pv        = p_ecef.pvector.ravel().tolist()  
+#         x_ecef[i] = pv[0]  
+#         y_ecef[i] = pv[1]  
+#         z_ecef[i] = pv[2] 
+#     return x_ecef,y_ecef,z_ecef 
 #_______________________________________________________________________________
 def enu2ecef(lat_deg:float,lon_deg:float,q_enu:np.array):
     # convert vector q in the East-North-Up system to that in the  

df_code/quiver_example.py
@@ -15,6 +15,16 @@ def my_func(x:np.ndarray,y:np.ndarray):
     f = x*np.exp(-(x**2+y**2)) 
     return f
 #_______________________________________________________________________________
+def get_vector_field():
+    x = np.arange(-1,1,0.1) 
+    y = np.arange(-1,1,0.1)
+    # create grid  
+    X,Y = np.meshgrid(x,y)
+    # evaluate function 
+    z   = my_func(X,Y) 
+    dX,dY = np.gradient(z)  
+    return X,Y,dX,dY 
+#_______________________________________________________________________________
 
 if __name__=='__main__':
 
@@ -23,15 +33,8 @@ if __name__=='__main__':
     plt.rcParams["figure.figsize"]    = [10,8]
     plt.rcParams["figure.autolayout"] = True
 
-    # x = np.arange(-1,1,0.1) 
-    # y = np.arange(-1,1,0.1) 
-
-    # X,Y = np.meshgrid(x,y) 
-    # z   = my_func(X,Y) 
-    # dX,dY = np.gradient(z)  
-    # 
-    # f = plt.figure()
-    # 
+    # X,Y,dX,dY = get_vector_field() 
+    # f  = plt.figure()
     # ax = f.add_subplot(polar=False)
     # ax.quiver(X,Y,dX,dY)
     # ax.set_aspect('equal') 
@@ -39,73 +42,60 @@ if __name__=='__main__':
     # ax.set_ylabel('y [arb]')  
 
     # defining a rotation matrix about the z axis (in ECEF)  
-    th = np.deg2rad(90.)  
+    th = np.deg2rad(-90.)  
     Mz = np.matrix([[np.cos(th),-np.sin(th),0],
                     [np.sin(th),np.cos(th),0],
                     [0,0,1]
                    ])
-    My = np.matrix([[np.cos(th),0,np.sin(th)],
-                    [0,1,0],
-                    [-np.sin(th),0,np.cos(th)]
-                   ])
-
- 
-    # MM = nv.rotation.R2xyz(M)
  
     lat = np.arange(60, 75)
     lon = np.zeros(lat.shape)
     alt = np.ones(lat.shape) * 800E3
-    z = -alt
+    z   = -alt
     deg = True
  
     # convert geodetic (lat,lon,z) to ECEF (x,y,z) using nvector
     wgs84 = nv.FrameE(name='WGS84')
-    N    = len(lat) - 1
-    XYZ  = np.zeros((N, 3))
-    UVW  = np.zeros((N, 3))
-    XYZR = np.zeros((N, 3))
-    UVWR = np.zeros((N, 3))
+    N     = len(lat) - 1
+    XYZ   = np.zeros((N, 3))
+    UVW   = np.zeros((N, 3))
+    UVWR  = np.zeros((N, 3))
+    UVWR2 = np.zeros((N, 3))
     for i in range(N):
         p  = wgs84.GeoPoint(latitude=lat[i],longitude=lon[i],z=z[i],degrees=deg)
         p2 = wgs84.GeoPoint(latitude=lat[i+1],longitude=lon[i+1],z=z[i+1],degrees=deg)
-        # compute the relative distance to the next (lat,lon,z) point  
-        brng      = p.delta_to(p2)     # note: can effectively turn this into a bearing angle by calling .azimuth_deg
-        brng_cpy  = brng     
-        # convert to ECEF frame 
-        p_ecef    = p.to_ecef_vector()
-        brng_ecef = brng.to_ecef_vector()
-        # rotate the vector 
-        # temp       = p_ecef.pvector[:].flatten() 
-        # temp_r     = M.dot(temp) 
-        # p_ecef_rot = nv.FrameE(name='WGS84').ECEFvector(temp_r)  
-        start     = p_ecef.pvector[:].flatten()
+        # compute the relative distance to the next (lat,lon,z) point 
+        # note: can effectively turn this into a bearing angle by calling .azimuth_deg
+        brng      = p.delta_to(p2)     
+        # rotate the vector
+        frame_B   = nv.FrameB(brng.to_nvector(),yaw=90,pitch=0,roll=0,degrees=deg)  
+        p_brng_B  = frame_B.Pvector(np.r_[1,0,0].reshape((-1,1))) # argument is a unit vector along the x axis 
+        # wrong way
+        brng_ecef = brng.to_ecef_vector() 
         temp      = brng_ecef.pvector[:].flatten()
         temp_r    = Mz.dot(temp)
-        # temp_r[0,1] -= temp_r[0,1]  
-        # temp_r    = My.dot(temp_r.reshape(3,1))
-        # temp_r    = np.ravel(temp_r)  
-        # # temp_r    = temp_r[0,:].flatten()
-        # print(type(temp))
-        # print(type(temp_r))
-        print('start: {0}, end: {1}, end after rotation: {2}'.format(start,temp,temp_r))  
-        brng_ecef_rot = nv.FrameE(name='WGS84').ECEFvector(temp_r) 
-        # brng_ecef_rot = brng_ecef.rotation.R_EL2n_E(M) # doesn't work  
+        brng_ecef_rot_wrong = nv.FrameE(name='WGS84').ECEFvector(temp_r)     
+        # convert to ECEF frame 
+        p_ecef        = p.to_ecef_vector()
+        brng_ecef     = brng.to_ecef_vector()
+        brng_ecef_rot = p_brng_B.to_ecef_vector()  
         # fill the output  
         XYZ[i,:]  = p_ecef.pvector[:].flatten()
         UVW[i,:]  = brng_ecef.pvector[:].flatten()
-        # XYZR[i,:] = p_ecef_rot.pvector[:].flatten()
         UVWR[i,:] = brng_ecef_rot.pvector[:].flatten()
+        UVWR2[i,:] = brng_ecef_rot_wrong.pvector[:].flatten()
  
     # breakpoint()
     ax = plt.figure().add_subplot(projection='3d')
     ax.quiver(*XYZ.T,*UVW.T)
     ax.quiver(*XYZ.T,*UVWR.T,color='red')
+    ax.quiver(*XYZ.T,*UVWR2.T,color='green')
 
     ax.set_xlabel('x') 
     ax.set_ylabel('y') 
     ax.set_zlabel('z') 
-    ax.set_xlim([0,4E+6])
+    # ax.set_xlim([0,4E+6])
     ax.set_ylim([-4,4])
-    ax.set_zlim([0,7E+6])
+    # ax.set_zlim([0,7E+6])
  
     plt.show()